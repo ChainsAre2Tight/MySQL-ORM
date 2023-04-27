c5d7353f6be546f5d0c951d0ac152e0caf9570b8
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2023-04-27 12:58:06 +0300

    added a new dataclass for comparing columns and fields

--- internals/dataobject.py
+++ internals/dataobject.py
@@ -14,3 +14,19 @@ class DataObject:
 
     def __str__(self) -> str:
         return str(self._data)
+
+
+class ColumnFieldData:
+    datatype: str
+    position: int
+
+    def __init__(self, d, p):
+        self.datatype = d
+        self.position = p
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, self.__class__):
+            if self.datatype == other.datatype and self.position == other.position:
+                return True
+            return False
+        raise NotImplementedError('cannot compare instances of different classes')
